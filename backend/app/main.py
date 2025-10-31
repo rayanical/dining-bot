5d4a624e1a569eff9b2d0fb8bca98da0f86672f5
rayan <87831066+rayanical@users.noreply.github.com>
2025-10-31 06:28:05 -0400

    Connected FastAPI backend to Frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import test
 
 app = FastAPI(title="Dining Bot API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(test.router, prefix="/api/test", tags=["Test"])
 
