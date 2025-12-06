58b5eaa6d87b274bc3cb0b4a2c5d38e67a7a4f3c
rayan <87831066+rayanical@users.noreply.github.com>
2025-12-05 20:19:19 -0500

    Delete migrations directory

--- migrations/003_add_protein_g_to_diet_history.sql
+++
@@ -1,6 +0,0 @@
--- Add protein_g column to diet_history if it does not exist
-ALTER TABLE diet_history
-    ADD COLUMN IF NOT EXISTS protein_g DOUBLE PRECISION;
-
--- Backfill existing rows with 0 where NULL
-UPDATE diet_history SET protein_g = 0 WHERE protein_g IS NULL;
